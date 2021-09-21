1d65062a51bc386b8252dfc5df98aaa6cff3ede2
Rogério Missias <missiasou@gmail.com>
2021-09-20 22:04:40 -0300

    aula 20-09

---
+++ Aula_valores_modulo2/index.js
@@ -0,0 +1,67 @@
+
+        let javascript = "Javascript é top";
+        if (javascript === "Javascript é top") alert("Javascript é mara!")
+        console.log(40 + 8+ 23 - 10);
+
+
+        //Valores e variáveis
+
+        console.log("Rogério");
+        console.log(49 - 5 + 62 -37);
+
+        let primeiroNome = "Roger";
+        console.log(primeiroNome);
+
+        let PI = 3.14; //usar letras maiusculas quando é uma constante
+
+        //Tipos de dados
+
+        //Primitivos e objetos
+
+        // Number: pra decimal e inteiro
+        // String: sequencia de carcteres; usada para texto
+        // boolean: lógico: true ou false; usado para tomada de decisão
+        // undefined: tipo de variável que não está ainda definida, ex: valor vazio
+        let ultimoNome
+        // null: tbm significa valor vazio
+
+        let javascriptEhMara = true;
+        console.log(javascriptEhMara);
+        console.log(typeof true);
+        console.log(typeof javascriptEhMara);
+        javascriptEhMara = "true"
+        console.log(typeof javascriptEhMara);
+        let ano;
+        console.log(ano);
+        console.log(typeof ano);
+        ano = 1993;
+        console.log(typeof ano);
+        console.log(typeof null);
+
+        // let, const, var
+
+        // let: a variável pode ser alterada
+        let idade = 37;
+        // const: constante, valor não pode ser mudado
+        const age = 34;
+        // var: como boa pratica é melhor não utilizar
+
+        // operadores básicos
+        // Operadores matemáticos
+        const idadeRogerio = 2021 - 1984;
+        console.log(idadeRogerio);
+
+        const x = 2 ** 3;
+        console.log(x);
+
+        const firstName = Rogerio
+        const lastName = Oliveira
+        console.log(firstName + lastName);
+
+        let y = 10 + 5;
+        y += 10; // y = y + 10
+        y +=  4; // y = y + 4
+
+
+
+
