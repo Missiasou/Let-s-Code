6f49237278965e8868aade16cc9fad45475b0f83
Rogério Missias <missiasou@gmail.com>
2021-09-22 22:17:23 -0300

    aula 22-09

--- Exercicios/ex_01-Javascript/ex01.js
+++ Exercicios/ex_01-Javascript/ex01.js
@@ -1,20 +1,29 @@
-let markPeso = 78;
-let markAltura = 1.69;
-let jonhPeso = 92;
-let jonhAltura = 1.95;
+const markPeso = 48;
+const markAltura = 1.69;
+const jonhPeso = 92;
+const jonhAltura = 1.95;
 
-let imcMark = markPeso/(markAltura*markAltura);
-let imcJonh = jonhPeso/(jonhAltura*jonhAltura);
+const imcMark = markPeso/(markAltura*markAltura);  // pode usar o toFixed(2) para arredondar.
+const imcJonh = jonhPeso/(jonhAltura*jonhAltura);
 
-let markMaiorImc;
+const markMaiorImc = imcMark > imcJonh
 
-if (imcMark > imcJonh){
-    console.log(`O IMC do Mark é maior`)
+if (markMaiorImc) {
+   console.log(`O IMC de Mark (${imcMark.toFixed(2)}) é maior que o de Jonh (${imcJonh.toFixed(2)})`);
+} else {
+   console.log(`O IMC de Jonh (${imcJonh.toFixed(2)}) é maior que o de Mark (${imcMark.toFixed(2)})`);
+}
+
+// console.log(markMaiorImc)
+
+// if (imcMark > imcJonh){
+//     console.log(`O IMC do Mark é maior`)
     
- } else {
-    console.log(`O IMC do Mark é menor`) 
- }
- console.log(`Mark tem IMC ${imcMark} e Jonh tem IMC ${imcJonh}`)
+//  } else {
+//     console.log(`O IMC do Mark é menor`) 
+//  }
+//  console.log(`Mark tem IMC ${imcMark} e Jonh tem IMC ${imcJonh}`)
 
 
- 
+//  const roger = 15;
+//  console.log(roger);
