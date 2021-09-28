6032d4be7653ec342afac123d8896095b070f27c
Rogério Missias <missiasou@gmail.com>
2021-09-27 21:16:00 -0300

    aula27-09

---
+++ Aulas_modulo2/aula04.js
@@ -0,0 +1,29 @@
+// // Desestruturação de arry
+// const arry = [2,3,4];
+
+// const [x, y, z] = arry;
+
+// console.log(x);
+
+// // operador spread ...arr
+
+// const menuRestaurante = ["Macarrão", "Sopa", "Sushi", "Pao"]
+
+// const newMenu = [...menuRestaurante, "Pizza"]
+
+// const menu = [...menuRestaurante,...newMenu];
+
+// console.log(menu);
+
+
+const rogerio = {
+    firstName: "Rogério",
+    lastName: "Oliveira",
+    age: 2021 -1984,
+    job: "Professor",
+    friends: ["Edivania", "Larissa", "Filipe"]
+}
+
+// console.log(`${rogerio.firstName} tem ${rogerio.friends.length} amigos e o melhor amigo é o ${rogerio.friends[1]}`);
+
+
