d8a56b5dd3965e835347c6f1b54f387f72c2687e
Rogério Missias <missiasou@gmail.com>
2021-10-28 22:06:19 -0300

    revisao aula 1 parte II

--- Aulas_modulo4/aula01/script.js
+++ Aulas_modulo4/aula01/script.js
@@ -22,6 +22,38 @@ document.querySelector('.check').addEventListener('click', function(){
             document.querySelector('.highscore').textContent = highscore;
         }
     } else if(guess > secretNumber){
+        if(score > 1){
+            document.querySelector('.message').textContent = "Você escolheu um número maior";
+            score--;
+            document.querySelector('.score').textContent = score;
+        } else{
+            document.querySelector('.message').textContent = "Você perdeu o jogo";
+            document.querySelector('.score').textContent = 0;
+        }
         
+        
+    } else if(guess < secretNumber){
+        if(score > 1){
+        document.querySelector('.message').textContent = "Você escolheu um número menor";
+        score--;
+        document.querySelector('.score').textContent = score;
+
+        }  else{
+            document.querySelector('.message').textContent = "Você perdeu o jogo";
+            document.querySelector('.score').textContent = 0;
+        }
     }
+})
+
+document.querySelector('.again').addEventListener('click', function(){
+    score = 20;
+    secretNumber = Math.trunc(Math.random()*20) + 1;
+    document.querySelector('.score').textContent = score;
+    document.querySelector('body').style.background = '#222';
+    document.querySelector('.number').style.width = '15rem';
+    document.querySelector('.number').textContent = '?';
+    document.querySelector('.message').textContent = "Comece a advinhar...";
+    document.querySelector('.guess').value = '';
+    
+
 })
