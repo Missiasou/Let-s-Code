31a272ad62d68d6cdb1fc44792040415dcb5f107
Rogério Missias <missiasou@gmail.com>
2021-10-30 23:36:22 -0300

    project caminhando

--- Aulas_modulo4/projeto1/script.js
+++ Aulas_modulo4/projeto1/script.js
@@ -1,39 +1,66 @@
 // 1 red, 2 orange, 3 yellow, 4 green, 5 blue, 6 purple, 7 pink, 
 const btn = document.querySelectorAll('.btn');
 const btnInicio = document.querySelector('.botao_inicio')
-const red = document.querySelector('.red')
-const orange = document.querySelector('.orange')
-const yellow = document.querySelector('.yellow')
-const green = document.querySelector('.green')
-const blue = document.querySelector('.blue')
-const purple = document.querySelector('.purple')
-const pink = document.querySelector('.pink')
-let coresEscolhidas = [];
 
 let musicGod = ['red', 'red', 'yellow', 'orange', 'orange', 'orange', 
 'green',  'yellow', 'yellow', 'yellow', 'blue', 'green', 'orange', 'green',
 'yellow', 'orange', 'red'];
 
+let count = 0;
+let tocando = musicGod[count];
+let tocandoMusica = [];
+
+
 
 
 document.querySelector('.botao_inicio').addEventListener('click', function(){
-   btn.forEach(btn => btn.classList.add('hidden'));
    btnInicio.classList.add('hidden');
-
-//    document.querySelector(`.${musicGod[0]}`).classList.remove('hidden')
-   
-sequenciaMusica(musicGod);
-     
+    animarBotao(tocando); 
+  
+    
 })
 
+function animarBotao(tocando) {
+    document.getElementById(tocando).classList.add("pressed");
 
-// sequenciaMusica();
-
-function sequenciaMusica(event) {
-    event.forEach((elem) => 
-    
-    
-        document.querySelector(`.${elem}`).classList.remove('hidden'))
- 
-   
+    setTimeout(function () {
+        document.getElementById(tocando).classList.remove("pressed");
+    }, 200)
 }
+
+
+function playSom(tocando) {
+    const audio = new Audio(`sounds/${tocando}.mp3`);
+
+    audio.play();
+}
+
+function proximaSequencia() {
+    
+    let tocar = musicGod[count];
+    console.log(tocar)
+    console.log(count)
+    
+    // // criar animação de piscar
+    // animarBotao(tocando);
+    // // criar som
+    // playSom(tocando);
+}
+
+document.querySelectorAll('.btn').forEach(btnEl => {
+    btnEl.addEventListener('click', function (event) {
+        const botaoclicado = event.target.id;
+        tocandoMusica.push(botaoclicado);
+        // console.log(tocandoMusica);
+        animarBotao(botaoclicado);
+        playSom(botaoclicado);
+        proximaSequencia(musicGod);
+        count++;
+
+        // checkar se a resposta está correta
+        // checar()
+
+    })
+});
+
+
